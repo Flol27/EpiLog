584c790dbca35a3e5668a92fca826f730c83970e
Quan <yuusching@gmail.com>
2026-06-29 16:15:27 +0200

    Add reviews system and fix migration

--- prisma/migrations/20260629140003_add_reviews/migration.sql
+++ prisma/migrations/20260629140003_add_reviews/migration.sql
@@ -1,14 +1,4 @@
 -- CreateTable
-CREATE TABLE "Friendship" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "fromId" INTEGER NOT NULL,
-    "toId" INTEGER NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "Friendship_fromId_fkey" FOREIGN KEY ("fromId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "Friendship_toId_fkey" FOREIGN KEY ("toId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
-
--- CreateTable
 CREATE TABLE "Review" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "userId" INTEGER NOT NULL,
@@ -20,9 +10,5 @@ CREATE TABLE "Review" (
     CONSTRAINT "Review_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT "Review_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "Friendship_fromId_toId_key" ON "Friendship"("fromId", "toId");
-
 -- CreateIndex
 CREATE UNIQUE INDEX "Review_userId_bookId_key" ON "Review"("userId", "bookId");
